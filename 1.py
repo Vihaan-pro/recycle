cfbe0b0747f131c5cd6fdd4637a9d0fdd96d52df
Vihaan-pro <vihaandagar23@gmail.com>
2024-12-19 19:03:51 +0100

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+from turtle import Screen
+import pygame 
+import random
+import time
+from pygame.locals import *
+pygame.init()
+screen = pygame.display.set_mode(900,700)
+b = pygame.image.load("9.png")
+screen.blit(b,(0,0))
